34e0b046620ec5747eeccd695f25ce00dda29df6
Januda Jinajith <janudajinajith.1@gmail.com>
2025-12-10 01:52:03 +0530

    Expenses curruncy change

--- front-end/my-frontend/src/expenses/ExpenseManagement.jsx
+++ front-end/my-frontend/src/expenses/ExpenseManagement.jsx
@@ -112,7 +112,7 @@ const ExpenseManagement = () => {
   const formatCurrency = (value) => {
     return new Intl.NumberFormat('en-US', {
       style: 'currency',
-      currency: 'USD'
+      currency: 'LKR'
     }).format(value || 0);
   };
 
