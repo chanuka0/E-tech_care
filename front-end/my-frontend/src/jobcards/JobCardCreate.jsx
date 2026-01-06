5d9ad2ccd6228f680d41c6ab177e5bf464b2c937
Januda Jinajith <janudajinajith.1@gmail.com>
2026-01-07 01:17:37 +0530

    job card add barcode remove

--- front-end/my-frontend/src/jobcards/JobCardCreate.jsx
+++ front-end/my-frontend/src/jobcards/JobCardCreate.jsx
@@ -971,7 +971,7 @@ const JobCardCreate = ({ onSuccess, onCancel }) => {
                     </svg>
                     <span className="ml-1">Add</span>
                   </button>
-                  <button
+                  {/* <button
                     type="button"
                     onClick={() => setShowDeviceBarcodeScanner(!showDeviceBarcodeScanner)}
                     className="bg-purple-600 hover:bg-purple-700 text-white font-medium py-2 px-4 rounded-md transition-colors flex items-center justify-center"
@@ -980,7 +980,7 @@ const JobCardCreate = ({ onSuccess, onCancel }) => {
                     <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4v1m6 11h2m-6 0h-2v4m0-11v3m0 0h.01M12 12h4.01M16 20h4M4 12h4m12 0h.01M5 8h2a1 1 0 001-1V5a1 1 0 00-1-1H5a1 1 0 00-1 1v2a1 1 0 001 1zm12 0h2a1 1 0 001-1V5a1 1 0 00-1-1h-2a1 1 0 00-1 1v2a1 1 0 001 1zM5 20h2a1 1 0 001-1v-2a1 1 0 00-1-1H5a1 1 0 00-1 1v2a1 1 0 001 1zm12 0h2a1 1 0 001-1v-2a1 1 0 00-1-1h-2a1 1 0 00-1 1v2a1 1 0 001 1z" />
                     </svg>
-                  </button>
+                  </button> */}
                 </div>
               </div>
 
@@ -1104,7 +1104,7 @@ const JobCardCreate = ({ onSuccess, onCancel }) => {
                     </svg>
                     <span className="ml-1">Add</span>
                   </button>
-                  <button
+                  {/* <button
                     type="button"
                     onClick={() => setShowOtherSerialScanner(!showOtherSerialScanner)}
                     className="bg-purple-600 hover:bg-purple-700 text-white font-medium py-2 px-4 rounded-md transition-colors flex items-center justify-center"
@@ -1113,7 +1113,7 @@ const JobCardCreate = ({ onSuccess, onCancel }) => {
                     <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4v1m6 11h2m-6 0h-2v4m0-11v3m0 0h.01M12 12h4.01M16 20h4M4 12h4m12 0h.01M5 8h2a1 1 0 001-1V5a1 1 0 00-1-1H5a1 1 0 00-1 1v2a1 1 0 001 1zm12 0h2a1 1 0 001-1V5a1 1 0 00-1-1h-2a1 1 0 00-1 1v2a1 1 0 001 1zM5 20h2a1 1 0 001-1v-2a1 1 0 00-1-1H5a1 1 0 00-1 1v2a1 1 0 001 1zm12 0h2a1 1 0 001-1v-2a1 1 0 00-1-1h-2a1 1 0 00-1 1v2a1 1 0 001 1z" />
                     </svg>
-                  </button>
+                  </button> */}
                 </div>
               </div>
 
